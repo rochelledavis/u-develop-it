0f9e6f190e90a4356a514de123bfa296d5650fa5
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-04-04 21:16:43 -0600

    database call for all rows

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,7 +9,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
